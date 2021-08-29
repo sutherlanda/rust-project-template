747f9d029d2166cd75ee395a3ae2013d45748067
Andrew Sutherland <sutherlanda@gmail.com>
2021-08-28 21:58:29 -0700

    nix: update nix repo rev/sha

--- my-project/src/main.rs
+++ my-project/src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
+    let _a = Some(1);
+    let b = "Test";
+    let _c = b.to_string();
     println!("Hello, world!");
-    println!("Hi");
 }
--- nix/default.nix
+++ nix/default.nix
@@ -5,10 +5,10 @@ let
   myLibs = import (systemPkgs.fetchFromGitHub {
     owner = "sutherlanda";
     repo = "nix-project";
-    rev = "c8c1e5642d5da8ce2165f9a142ce9dcf8d20e06b";
-    sha256 = "10c759m65wj3hwj03nfcs8rz0q6mvb3n8bvsk7b1yhipy5002m20";
+    rev = "f30835fc907d9c06d91587af33aa22489e7cb082";
+    sha256 = "10bb9swvcgixa8yvnxjwhqjsj5gpq9vi5lg3iva85ki75l7g1rr6";
   });
-  # myLibs = import ../../nix-project/default.nix;
+  #myLibs = import ../../nix-project/default.nix;
   pkgs = myLibs.config.pkgSet;
 in
   rec {
